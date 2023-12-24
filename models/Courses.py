0da5c13b619eb2c57c0e6036551d73d5e9de5b76
Yassine EL KHANTACH <yassine.elkhantach@hotmail.com>
2023-12-24 03:12:37 +0100

    fixed fixed the previous issue by just specifying the name of module, you should just rename your module folder 'e_courses', i've also documented my code by comments

--- models/Courses.py
+++ models/Courses.py
@@ -1,36 +1,43 @@
 from odoo import models, fields, api
 
 class Course(models.Model):
-    _name = 'courses.course'
+    _name = 'e_courses.course'
     _description = 'Courses'
 
+    # Fields for Course model
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
-    instructor_ids = fields.Many2many('res.partner', string='Instructors')
     price = fields.Float(string='Course Price')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('in progress', 'Ongoing'),
-        ('completed', 'Completed')],default='draft')
+        ('completed', 'Completed')], default='draft')
 
-    tags = fields.Many2many('custom_module.course.tag', string='Tags')
+    # Many-to-many relationship with CourseTag model
+    tags = fields.Many2many('e_courses.course.tag', string='Tags')
+
+    # Binary field for storing course photo
     photo = fields.Binary(string='Photo')
 
+    # Override create method to add custom logic
     @api.model
     def create(self, values):
         course = super(Course, self).create(values)
         return course
 
+    # Custom method to transition course to 'in progress' state
     def start_course(self):
         self.write({'state': 'in progress'})
 
+    # Custom method to transition course to 'completed' state
     def complete_course(self):
         self.write({'state': 'completed'})
 
 class CourseTag(models.Model):
-    _name = 'courses.course.tag'
+    _name = 'e_courses.course.tag'
     _description = 'Course Tags'
 
+    # Field for CourseTag model
     name = fields.Char(string='Tag Name', required=True)
